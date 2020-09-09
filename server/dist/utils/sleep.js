8d1c0c1e1f80c4990ce7aef0bfd7d2b6685069bd
foyez <foyezar@gmail.com>
2020-09-10 00:11:32 +0600

    remove dist directory from server

--- server/dist/utils/sleep.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sleep = void 0;
-exports.sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-//# sourceMappingURL=sleep.js.map
